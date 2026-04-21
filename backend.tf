1fe51bcc049305ae57957c4dd393653f3cd726ee
luvlibin <49586789+luvlibin@users.noreply.github.com>
2026-04-21 17:40:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "libin-demos3bucket"
     key            = "ecs-vpc/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform"
   }
 }
